4a61734b1001dbdd4944f65888b10905370f9861
Der_Googler <jimmyboehm@dergoogler.com>
2023-02-11 11:59:03 +0100

    Added Build.LOCKED

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-const {System} =require(".")
+const { System, SystemProperties, Build } = require(".")
 
 // console.log(System.uname())
 
 // Should throw error
-System.cmd("cmd battery reset -f")
+// System.cmd("cmd battery reset -f")
+
+const locked = SystemProperties.get("ro.boot.flash.locked")
+
+console.log(Build.LOCKED)
